166453183638311c70841da47e37584e2152b146
pykao <poyu_kao@ucsb.edu>
2019-03-19 00:46:10 -0700

    82.55

--- predict_gf.py
+++ predict_gf.py
@@ -53,30 +53,29 @@ def get_group_labels(labels, number_of_group):
 
 # training file path
 train_btsv_file = '/home/pkao/abcd/trainData/btsv01.txt'
-train_fi_file = '/home/pkao/abcd/trainData/training_fluid_intelligenceV1.csv'
+train_fi_file = '/home/pkao/abcd/trainData/results/training_fluid_intelligenceV1.csv'
 
 # validation file path
 valid_btsv_file = '/home/pkao/abcd/validData/btsv01.txt'
-valid_fi_file = '/home/pkao/abcd/validData/validation_fluid_intelligenceV1.csv'
+valid_fi_file = '/home/pkao/abcd/validData/results/validation_fluid_intelligenceV1.csv'
 
 # test file path
-test_btsv_file = '/media/hdd3/abcd_data/testData/btsv01.txt'
+test_btsv_file = '/home/pkao/abcd/testData/btsv01.txt'
 
 ############################### check if the provided files exists or not #####################
 
-assert exists(train_btsv_file),"btsv01.txt of training set does not exist!!!"
-assert exists(train_fi_file),"training_fluid_intelligenceV1.csv does not exist!!!"
-assert exists(valid_btsv_file),"btsv01.txt of validation set does not exist!!!"
-assert exists(valid_fi_file),"validation_fluid_intelligenceV1.csv does not exist!!!"
-assert exists(test_btsv_file),"btsv01.txt of testing set does not exist!!!"
+assert os.path.exists(train_btsv_file),"btsv01.txt of training set does not exist!!!"
+assert os.path.exists(train_fi_file),"training_fluid_intelligenceV1.csv does not exist!!!"
+assert os.path.exists(valid_btsv_file),"btsv01.txt of validation set does not exist!!!"
+assert os.path.exists(valid_fi_file),"validation_fluid_intelligenceV1.csv does not exist!!!"
+assert os.path.exists(test_btsv_file),"btsv01.txt of testing set does not exist!!!"
 
 ###############################################################################################################
 
 seed = 1989
 
 dropped_names = ['collection_id', 'btsv01_id', 'dataset_id', 'subjectkey', 'src_subject_id', 
-'interview_date', 'collection_title', 'promoted_subjectkey', 'study_cohort_name', 'subject',
-'interview_age', 'gender']
+'interview_date', 'collection_title', 'study_cohort_name', 'subject', 'interview_age', 'sex']
 
 ############################################# Training Dataset #################################################
 
@@ -84,28 +83,30 @@ df_train_btsv = pd.read_csv(train_btsv_file, delim_whitespace=True, low_memory=F
 df_train_fi = pd.read_csv(train_fi_file)
 merged_train_df = pd.merge(df_train_btsv, df_train_fi, how='inner', left_on='subjectkey', right_on='subject')
 
+
+
 # histogram of fluid intelligence of training subjects
 #fig, ax = plt.subplots()
 #df_train_fi.hist(column='residual_fluid_intelligence_score', bins=100, ax=ax)
 #fig.savefig('training.png')
 
-df_train_age_gender = merged_train_df[['interview_age', 'gender']]
+df_train_age_gender = merged_train_df[['interview_age', 'sex']]
 df_train_age_gender = pd.get_dummies(df_train_age_gender)
 
 df_train = merged_train_df.drop(dropped_names, axis=1)
 
 # Ground-truth of the fluid intelligence score
-train_labels = np.array(df_train['residual_fluid_intelligence_score'])
-#print(np.mean(train_labels))
+train_labels = df_train['residual_fluid_intelligence_score'].values
+#print(np.mean(train_labels), np.std(train_labels))
 
 # training dataset in dataframe
 df_train = df_train.drop('residual_fluid_intelligence_score', axis = 1)
 
 # feature list and features in numpy matrix
 feature_list = list(df_train.columns)
-train_features = np.array(df_train)
-train_age_gender = np.array(df_train_age_gender)
-
+train_features = df_train.values
+train_age_gender = df_train_age_gender.values
+#print(train_features.dtype, train_features.shape)
 
 ############################################# Validation Dataset #################################################
 
@@ -118,63 +119,27 @@ merged_valid_df = pd.merge(df_valid_btsv, df_valid_fi, how='inner', left_on='sub
 #df_valid_fi.hist(column='residual_fluid_intelligence_score', bins=100, ax=ax)
 #fig.savefig('validation.png')
 
-df_valid_age_gender = merged_valid_df[['interview_age', 'gender']]
+df_valid_age_gender = merged_valid_df[['interview_age', 'sex']]
 df_valid_age_gender = pd.get_dummies(df_valid_age_gender)
 
 df_valid = merged_valid_df.drop(dropped_names, axis=1)
 
-valid_labels = np.array(df_valid['residual_fluid_intelligence_score'])
-#print(np.mean(valid_labels))
+valid_labels = df_valid['residual_fluid_intelligence_score'].values
+#print(np.mean(valid_labels), np.std(valid_labels))
 
 df_valid = df_valid.drop('residual_fluid_intelligence_score', axis = 1)
 
-valid_features = np.array(df_valid)
-valid_age_gender = np.array(df_valid_age_gender)
+valid_features = df_valid.values
+
+valid_age_gender = df_valid_age_gender.values
 
 
 df_valid_subject_name = merged_valid_df[['subject']]
 
 
-############################################ Training + Validation #################################################
-
-
-df_train_valid_fi = pd.concat([df_train_fi, df_valid_fi])
-df_train_valid = pd.concat([df_train, df_valid])
-train_valid_labels = np.concatenate((train_labels, valid_labels), axis=0)
-train_valid_features = np.array(df_train_valid)
-#print(np.mean(train_valid_labels))
-
-'''
-# histogram of fluid intelligence of training + validation subjects
-#fig, ax = plt.subplots()
-#df_train_valid_fi.hist(column='residual_fluid_intelligence_score', bins=100, ax=ax)
-#fig.savefig('train_valid.png')
-
-#print('Normalizing features...')
-scaler = StandardScaler()
-normalized_train_valid_features = scaler.fit_transform(train_valid_features)
-#print(normalized_train_valid_features.shape[1])
-
-#print('Performing feature dimension reduction...')
-pca = PCA(n_components='mle', svd_solver='full', random_state=seed)
-pca_normalized_train_valid_features = pca.fit_transform(normalized_train_valid_features)
-#print(pca_normalized_train_valid_features.shape[1])
-
-#print('Removing features with low variance...')
-sel = VarianceThreshold(0.5*(1-0.5))
-high_variance_pca_normalized_train_valid_features = sel.fit_transform(pca_normalized_train_valid_features)
-#high_variance_feature_list = [name for idx, name in enumerate(feature_list) if sel.get_support()[idx]]
-#print(high_variance_pca_normalized_train_valid_features.shape[1])
-
-#print('Performing feature selection...')
-#print('Univariate Selection...')
-skb = SelectKBest(f_regression, k=24)
-selected_high_variance_pca_normalized_train_valid_features = skb.fit_transform(high_variance_pca_normalized_train_valid_features, train_valid_labels)
-'''
-
 ######################################## Data Pre-processing #####################################################
 
-
+'''
 print('Normalizing features...')
 scaler_1 = StandardScaler()
 normalized_train_features = scaler_1.fit_transform(train_features)
@@ -204,11 +169,51 @@ skb_1 = SelectKBest(f_regression, k=24)
 selected_high_variance_pca_normalized_train_features = skb_1.fit_transform(high_variance_pca_normalized_train_features, train_labels)
 selected_high_variance_pca_normalized_valid_features = skb_1.transform(high_variance_pca_normalized_valid_features)
 
+'''
+############################################ Training + Validation #################################################
+
+
+df_train_valid_fi = pd.concat([df_train_fi, df_valid_fi])
+df_train_valid = pd.concat([df_train, df_valid])
+train_valid_labels = np.concatenate((train_labels, valid_labels), axis=0)
+train_valid_features = np.array(df_train_valid)
+#print(np.mean(train_valid_labels), np.std(train_valid_labels))
+
+# histogram of fluid intelligence of training + validation subjects
+#fig, ax = plt.subplots()
+#df_train_valid_fi.hist(column='residual_fluid_intelligence_score', bins=100, ax=ax)
+#fig.savefig('train_valid.png')
+
+######################################## Data Pre-processing #####################################################
+
+
+#print('Normalizing features...')
+scaler = StandardScaler()
+normalized_train_valid_features = scaler.fit_transform(train_valid_features.astype('float64'))
+#print(normalized_train_valid_features.shape[1])
+
+#print('Performing feature dimension reduction...')
+pca = PCA(n_components='mle', svd_solver='full', random_state=seed)
+pca_normalized_train_valid_features = pca.fit_transform(normalized_train_valid_features)
+#print(pca_normalized_train_valid_features.shape[1])
+
+#print('Removing features with low variance...')
+sel = VarianceThreshold(0.5*(1-0.5))
+high_variance_pca_normalized_train_valid_features = sel.fit_transform(pca_normalized_train_valid_features)
+#high_variance_feature_list = [name for idx, name in enumerate(feature_list) if sel.get_support()[idx]]
+#print(high_variance_pca_normalized_train_valid_features.shape[1])
+
+#print('Performing feature selection...')
+#print('Univariate Selection...')
+skb = SelectKBest(f_regression, k=24)
+selected_high_variance_pca_normalized_train_valid_features = skb.fit_transform(high_variance_pca_normalized_train_valid_features, train_valid_labels)
+
 
 
 ############################################### StackNet #################################################
 
-
+'''
+### training, validation
 models=[
        ###### First Level ########## 
        [
@@ -225,51 +230,51 @@ models=[
        RandomForestRegressor(n_estimators=800, max_depth=3, n_jobs=-1, random_state=seed),
        ],
        ]
-
-
 '''
+
+### training + validation
 models=[
        ###### First Level ########## 
        [
        KernelRidge(alpha=512),
-       MLPRegressor(hidden_layer_sizes=(2), solver='adam', max_iter=10000, random_state=seed),
-       KNeighborsRegressor(n_neighbors=330,  weights='uniform', n_jobs=-1),
-       RandomForestRegressor(n_estimators=800, max_depth=13, random_state=seed, n_jobs=-1),
+       #MLPRegressor(hidden_layer_sizes=(2), solver='adam', max_iter=10000, random_state=seed),
+       #KNeighborsRegressor(n_neighbors=330,  weights='uniform', n_jobs=-1),
+       RandomForestRegressor(n_estimators=800, max_depth=11, random_state=seed, n_jobs=-1),
        RandomForestRegressor(n_estimators=1000, max_depth=7, random_state=seed, n_jobs=-1),
-       ExtraTreesRegressor(n_estimators=300, max_depth=13, random_state=seed, n_jobs=-1),
+       ExtraTreesRegressor(n_estimators=1800, max_depth=9, random_state=seed, n_jobs=-1),
+       GradientBoostingRegressor(n_estimators=40, max_depth=3, random_state=seed),
        GradientBoostingRegressor(n_estimators=20, max_depth=5, random_state=seed),
-       GradientBoostingRegressor(n_estimators=30, max_depth=3, random_state=seed),
        ],
        ####### Second Level #######
        [
-       RandomForestRegressor(n_estimators=1000, max_depth=3, n_jobs=-1, random_state=seed),
-       ExtraTreesRegressor(n_estimators=3000, max_depth=15, random_state=seed, n_jobs=-1),
+       RandomForestRegressor(n_estimators=1000, max_depth=9, n_jobs=-1, random_state=seed),
+       ExtraTreesRegressor(n_estimators=1800, max_depth=9, random_state=seed, n_jobs=-1),
+       BayesianRidge()
        ],
        [
        Ridge(alpha=512, random_state=seed)
        ]
        ]
-'''
 
 ############################################# Five-fold Cross-Validation ################################
 
 # divided subjects into differet groups with approximately same numbe of subjects
-number_of_group = 10
-group_labels = get_group_labels(train_labels, number_of_group)
-#group_labels = get_group_labels(train_valid_labels, number_of_group)
+number_of_group = 60
+#group_labels = get_group_labels(train_labels, number_of_group)
+group_labels = get_group_labels(train_valid_labels, number_of_group)
 
 # Select which feature to use
 #X = np.concatenate((selected_normalized_high_variance_features, normalized_age_gender), axis=1)
-X = selected_high_variance_pca_normalized_train_features
-#X = selected_high_variance_pca_normalized_train_valid_features
+#X = selected_high_variance_pca_normalized_train_features
+X = selected_high_variance_pca_normalized_train_valid_features
 #X = normalized_age_gender
-y = train_labels
-#y = train_valid_labels
+#y = train_labels
+y = train_valid_labels
 
-#np.save('X.npy', X)
-#np.save('y.npy', y)
+np.save('X.npy', X)
+np.save('y.npy', y)
 
-cv_fold = 5
+cv_fold = 10
 y_mse = np.zeros(cv_fold)
 skf = StratifiedKFold(n_splits=cv_fold,shuffle=True,random_state=seed)
 
@@ -280,17 +285,20 @@ for train_idx, test_idx in skf.split(X, group_labels):
 	X_train, X_test = X[train_idx], X[test_idx]
 	y_train, y_test = y[train_idx], y[test_idx]
 
+	#print(np.mean(y_train), np.std(y_train))
+	#print(np.mean(y_test), np.std(y_test))
+	
 	# regressor
 	#regr = LinearSVR(random_state=seed)
-	#regr = SVR(gamma='scale', C=0.25)
-	#regr = Ridge(alpha=512, random_state=seed)
+	#regr = SVR(gamma='scale', C=float(sys.argv[1]))
+	#regr = Ridge(alpha=int(sys.argv[1]), random_state=seed)
 	#regr = LinearRegression(n_jobs=-1)
 	#regr = RandomForestRegressor(n_estimators=int(sys.argv[1]), max_depth=15, random_state=seed, n_jobs=-1)
 	#regr = AdaBoostRegressor(DecisionTreeRegressor(max_depth=7), n_estimators=int(sys.argv[1]), random_state=seed)
-	#regr = XGBRegressor(n_estimators=10, max_depth=7, n_jobs=-1, random_state=seed)
+	#regr = XGBRegressor(n_estimators=int(sys.argv[1]), max_depth=5, n_jobs=-1, random_state=seed)
 	#regr = ExtraTreesRegressor(n_estimators=int(sys.argv[1]), max_depth=15, random_state=seed, n_jobs=-1)	
 	#regr = ExtraTreesRegressor(n_estimators=1800, max_depth=7, random_state=seed, n_jobs=-1)
-	#regr = GradientBoostingRegressor(n_estimators=int(sys.argv[1]), max_depth=7, random_state=seed)
+	#regr = GradientBoostingRegressor(n_estimators=int(sys.argv[1]), max_depth=5, random_state=seed)
 	#regr = GradientBoostingRegressor(n_estimators=10, max_depth=7, random_state=seed)
 	#regr = Lasso(alpha=0.001)
 	#regr = ElasticNet(alpha=0.001)
@@ -301,7 +309,8 @@ for train_idx, test_idx in skf.split(X, group_labels):
 	#regr = KNeighborsRegressor(n_neighbors=int(sys.argv[1]), weights='uniform', n_jobs=-1)
 	#regr = MLPRegressor(hidden_layer_sizes=(2), solver='adam', max_iter=10000, random_state=seed)
 	
-	regr = StackNetRegressor(models, metric="rmse", folds=4, restacking=True, use_retraining=True, random_state=seed, n_jobs=-1, verbose=1)
+
+	regr = StackNetRegressor(models, metric="rmse", folds=5, restacking=True, use_retraining=True, random_state=seed, n_jobs=-1, verbose=1)
 	regr.fit(X_train, y_train)
 	
 	y_pred = regr.predict(X_test)
@@ -315,7 +324,7 @@ print("Training with %d fold cross-validation, Mean squared error: %.4f" %(cv_fo
 
 print("#############################################################################################")
 
-
+exit()
 #################################### Training and Validation ##########################################
 X_train = selected_high_variance_pca_normalized_train_features
 y_train = train_labels
